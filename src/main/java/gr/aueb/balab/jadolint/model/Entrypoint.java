dc79c07b36e1a7eca5522f2ee18b975109c0aaba
Konstantinos Kravvaritis <blueoly@gmail.com>
2019-02-04 02:45:46 +0200

    add instructions to the model

---
+++ src/main/java/gr/aueb/balab/jadolint/model/Entrypoint.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gr.aueb.balab.jadolint.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author blue
+ */
+public class Entrypoint {
+    
+    private String executable;
+    private List<String> params = new ArrayList<>();
+    
+    public Entrypoint(String line){
+        
+        String lineWithoutInstruction = line.split(" ", 2)[1];
+        
+        if (lineWithoutInstruction.contains("[")) {
+            Pattern p = Pattern.compile("\"(.*?)\"");
+            Matcher m = p.matcher(lineWithoutInstruction);
+
+            List<String> matches = new ArrayList<String>();
+            while (m.find()) {
+                matches.add(m.group(1));
+            }
+
+            for (int i = 0; i < matches.size(); i++) {
+                if (i == 0) {
+                    executable = matches.get(i);
+                } else {
+                    params.add(matches.get(i));
+                }
+            }
+        } else {
+            String[] parts = lineWithoutInstruction.split(" ");
+
+            for (int i = 0; i < parts.length; i++) {
+                if (i == 0) {
+                    executable = parts[i];
+
+                } else {
+                    params.add(parts[i]);
+                }
+            }
+        }
+    }
+
+    public String getExecutable() {
+        return executable;
+    }
+
+    public List<String> getParams() {
+        return params;
+    }
+
+    public void setExecutable(String executable) {
+        this.executable = executable;
+    }
+
+    public void setParams(List<String> params) {
+        this.params = params;
+    }
+}
