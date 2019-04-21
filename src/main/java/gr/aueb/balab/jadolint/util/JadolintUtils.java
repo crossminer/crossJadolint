ba718d14aeb4a8281b8b32cac74304d9a87b26fe
Theodosis Tsaklanos <44101924+Ethelom@users.noreply.github.com>
2019-04-21 21:59:11 +0300

    Update and rename src/main/java/gr/aueb/balab/jadolnt/util/JadolintUtils.java to src/main/java/gr/aueb/balab/jadolint/util/JadolintUtils.java

--- src/main/java/gr/aueb/balab/jadolint/util/JadolintUtils.java
+++ src/main/java/gr/aueb/balab/jadolint/util/JadolintUtils.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package gr.aueb.balab.jadolnt.util;
+package gr.aueb.balab.jadolint.util;
 
 /**
  *
