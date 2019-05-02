527404dee781a9c1c8265222ae48260e52c96e66
Konstantinos Kravvaritis <blueoly@gmail.com>
2019-05-02 16:36:24 +0300

    add rule DL3024

---
+++ src/main/java/gr/aueb/balab/jadolint/rules/FromRules.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gr.aueb.balab.jadolint.rules;
+
+import gr.aueb.balab.jadolint.model.Dockerfile;
+import gr.aueb.balab.jadolint.model.From;
+import gr.aueb.balab.jadolint.model.Line;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author blue
+ */
+public class FromRules implements Rule {
+    private From from;
+    
+    public boolean checkDL3024(Dockerfile doc, int lineNumber){
+        
+        if(from.getAlias() != null){
+            for(Line l : doc.getLines()){
+                if(l.getLineNumber() != lineNumber){
+                    if(l.getInstruction().equals("FROM")){
+                        From currentFrom = new From(l.getLine());
+                        if(currentFrom.getAlias() !=  null)
+                            if(from.getAlias().equals(currentFrom.getAlias()))
+                                return false;
+                    }
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+    public FromRules(From from){
+        this.from = from;
+    }
+    
+}
