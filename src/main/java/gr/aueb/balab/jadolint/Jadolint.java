3a0fb7deca66a9cfd92f332ae36f2043f1e89d08
Konstantinos Kravvaritis <blueoly@gmail.com>
2019-05-07 04:43:35 +0300

    add main method in Jadolint class

--- src/main/java/gr/aueb/balab/jadolint/Jadolint.java
+++ src/main/java/gr/aueb/balab/jadolint/Jadolint.java
@@ -5,10 +5,29 @@
  */
 package gr.aueb.balab.jadolint;
 
+import gr.aueb.balab.jadolint.model.Add;
+import gr.aueb.balab.jadolint.model.Cmd;
+import gr.aueb.balab.jadolint.model.Copy;
 import gr.aueb.balab.jadolint.model.Dockerfile;
+import gr.aueb.balab.jadolint.model.Expose;
+import gr.aueb.balab.jadolint.model.From;
 import gr.aueb.balab.jadolint.model.Line;
+import gr.aueb.balab.jadolint.model.Maintainer;
+import gr.aueb.balab.jadolint.model.Run;
+import gr.aueb.balab.jadolint.model.Workdir;
+import gr.aueb.balab.jadolint.rules.AddRules;
+import gr.aueb.balab.jadolint.rules.CmdRules;
+import gr.aueb.balab.jadolint.rules.CopyRules;
+import gr.aueb.balab.jadolint.rules.ExposeRules;
+import gr.aueb.balab.jadolint.rules.FromRules;
+import gr.aueb.balab.jadolint.rules.MaintainerRules;
+import gr.aueb.balab.jadolint.rules.RunRules;
+import gr.aueb.balab.jadolint.rules.WorkdirRules;
+import gr.aueb.jadolint.violations.Violation;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -30,17 +49,52 @@ public class Jadolint {
             LineMerger l = new LineMerger();
             Dockerfile doc = new Dockerfile();
             
-            l.mergeLines(doc, new File("/Users/blue/Desktop/yeah/Dockerfile"));
+            doc.setPath(path);
+            
+            l.mergeLines(doc, new File(path));
+            
+            List<Violation> violations = new ArrayList<>();
             
             for(Line line : doc.getLines()){
                 //System.out.println(line.getLine() + " " + line.getLineNumber() + " " + line.getInstruction());
                 if(line.getInstruction().equals("ADD")){
-                    Add a = new Add(line.getLine());
-                    //RunRules r = new RunRules(new Run(line.getLine()));
-                    //System.out.println(r.checkDL3003() + " " + line.getLineNumber());
-                    System.out.println(a.getUser() + " " + a.getGroup() + " " + a.getSrc() + " " + a.getDest());
+                    AddRules addRules = new AddRules(new Add(line.getLine()));
+                    violations = addRules.runAddRules(doc, line.getLineNumber());
                 }
+                if(line.getInstruction().equals("CMD")){
+                    CmdRules cmdRules = new CmdRules(new Cmd(line.getLine()));
+                    violations = cmdRules.runCmdRules(doc, line.getLineNumber());
+                }
+                if(line.getInstruction().equals("COPY")){
+                    CopyRules copyRules = new CopyRules(new Copy(line.getLine()));
+                    violations = copyRules.runCopyRules(doc, line.getLineNumber());
+                }
+                if(line.getInstruction().equals("EXPOSE")){
+                    ExposeRules exposeRules = new ExposeRules(new Expose(line.getLine()));
+                    violations = exposeRules.runExposeRules(doc, line.getLineNumber());
+                }
+                if(line.getInstruction().equals("FROM")){
+                    FromRules fromRules = new FromRules(new From(line.getLine()));
+                    violations = fromRules.runFromRules(doc, line.getLineNumber());
+                }
+                if(line.getInstruction().equals("MAINTAINER")){
+                    MaintainerRules maintainerRules = new MaintainerRules(new Maintainer(line.getLine()));
+                    violations = maintainerRules.runMaintainerRules(doc, line.getLineNumber());
+                }
+                if(line.getInstruction().equals("RUN")){
+                    RunRules runRules = new RunRules(new Run(line.getLine()));
+                    violations = runRules.runRunRules(doc, line.getLineNumber());
+                }
+                if(line.getInstruction().equals("WORKDIR")){
+                    WorkdirRules workdirRules = new WorkdirRules(new Workdir(line.getLine()));
+                    violations = workdirRules.runWorkdirRules(doc, line.getLineNumber());
+                }
+                if(!violations.isEmpty())
+                    doc.addViolations(violations);
             }
+            
+            for(Violation v : doc.getViolations())
+                System.out.println(v.getFileName() + " " + v.getLineNumber() + " " + v.getCode() + " " + v.getMessage());
         } catch (IOException ex) {
             Logger.getLogger(Jadolint.class.getName()).log(Level.SEVERE, null, ex);
         }
